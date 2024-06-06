e45e4d2a00c902ac54db5ce21dcc9c87f694a6b9
Miloš Aksentijević <milos.aksentijevic@UC-C02XG0C6JG5M.local>
2024-06-06 21:58:33 +0200

    Changed CSS almost deleted everything

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,25 +4,6 @@ from .models import Question
 from django.template import loader
 
 
-def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
-
-def room(request):
-    return render(request, "room.html")
 
 def login(request):
     return render(request, "login.html")
-
-def results(request, question_id):
-    response = "You are looking at the result of %s"
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "index.html", context)
