f6a10dda94e5d4b7bf0b7a1ce8c25f7f934c980c
Shmay <miloss.kgg@gmail.com>
2024-06-06 22:10:57 +0200

    Pc commit

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
+
+
+def login(request):
+    return render(request, "login.html")
