3ea09f2e35034b2c033714d8f724339b1b74434c
Shmay <miloss.kgg@gmail.com>
2024-06-06 22:06:29 +0200

    as

--- firstProject/helloWolrd/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HellowolrdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'helloWolrd'
--- firstProject/helloWorld/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HellowrorldConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'helloWorld'
