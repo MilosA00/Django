43658588f25fdef78a2571fe7575ece5a64cf779
Miloš Aksentijević <milos.aksentijevic@UC-C02XG0C6JG5M.local>
2024-06-18 16:34:48 +0200

    Connected database and added user

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
 
-    path("login/", views.login, name="login"),
+    path("home", views.home, name="home"),
+    path("user/login", views.login, name="login"),
+    path("user/add", views.add_user, name="add_user"),
 
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
 from django.template import loader
-
+from .forms import UserForm
+from .models import User
+from django.views.decorators.csrf import csrf_exempt
 
 
 def login(request):
     return render(request, "login.html")
+
+
+@csrf_exempt
+def add_user(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("OK")
+        else:
+            return HttpResponse(request, status=400)
+
+
+def home(request):
+    users = User.objects.all()
+    return render(request, "home.html", {"users": users})
