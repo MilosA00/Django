6c397e98a9561c0131c3d34eac659605b121e027
Miloš Aksentijević <milos.aksentijevic@UC-C02XG0C6JG5M.local>
2024-06-19 17:24:59 +0200

    Returning data from login form

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path("home", views.home, name="home"),
     path("user/login", views.login, name="login"),
+    path("user/login_request", views.login_request, name="login_request"),
     path("user/add", views.add_user, name="add_user"),
 
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,10 +4,45 @@ from django.template import loader
 from .forms import UserForm
 from .models import User
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 
+@csrf_exempt
 def login(request):
     return render(request, "login.html")
+
+
+@csrf_exempt
+def login_request(request):
+    if request.method == "POST":
+        user = User.objects.all()
+        # user.authenticate(user_name="123", password="123")
+
+        for i in range(len(user)):
+            if user[i].user_name == request.get("user_name"):
+                if user[i].password == request.POST['password']:
+                    return redirect("home")
+
+        print(user.password)
+        if user is not None:
+            return redirect("home")
+        else:
+            return HttpResponse("Invalid login form")
+        for user in user:
+            print(form.__dict__)
+            print(request.POST.__dict__)
+            if user.user_name == request.POST.get('username', ""):
+                if user.password == request.POST.get('password', ""):
+                    return redirect("home")
+
+        # if user.("user_name") == request.user.username:
+        #     return redirect("home.html")
+        # else:
+        #     return HttpResponse("Username or password incorrect")
+
+    form = UserForm()
+    return render(request, "login.html", {"form": form})
 
 
 @csrf_exempt
